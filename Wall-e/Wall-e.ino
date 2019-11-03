f6b73d9cb6f1cc7f6d74011196620817c8dcd1da
Hung Che <chedaihung4864@gmail.com>
2019-11-03 15:11:48 -0600

    Update Wall-e.ino

--- Wall-e/Wall-e.ino
+++ Wall-e/Wall-e.ino
@@ -1,13 +1,30 @@
 #include <Servo.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7,8);
+const byte address[6] = "00001";
 Servo left;
 Servo right;
+int joy[2]={90,90};
 
-void setup() {
-  left.attach(9);
-  right.attach(8);
+void setup() {  
+  left.attach(3);
+  right.attach(10);
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
-
 void loop() {
-  left.write(80);
-  right.write(100);
+  if(radio.available()){
+    Serial.println(joy[1]);
+    radio.read(&joy, sizeof(joy));
+    left.write(joy[0]);
+    right.write(joy[1]);
+  }else{
+    Serial.println("no");
+  }
 }
