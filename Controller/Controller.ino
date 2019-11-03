f478ca24c2d3481532b5ecb3488d7d8db91bd1ac
Hung Che <chedaihung4864@gmail.com>
2019-11-03 15:11:57 -0600

    Update Controller.ino

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,31 +1,28 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#define led 12
+
 RF24 radio(7, 8); // CE, CSN
-const byte addresses[][6] = {"00001", "00002"};
-boolean buttonState = 0;
+const byte addresses[6] = "00001";
+
 void setup() {
-  pinMode(12, OUTPUT);
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(2, INPUT_PULLUP); 
   radio.begin();
-  radio.openWritingPipe(addresses[1]); // 00002
-  radio.openReadingPipe(1, addresses[0]); // 00001
+  radio.openWritingPipe(addresses); 
   radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
 void loop() {
-  delay(5);
-  radio.stopListening();
-  int potValue = analogRead(A0);
-  int angleValue = map(potValue, 0, 1023, 0, 180);
-  radio.write(&angleValue, sizeof(angleValue));
-  delay(5);
-  radio.startListening();
-  while (!radio.available());
-  radio.read(&buttonState, sizeof(buttonState));
-  if (buttonState == HIGH) {
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led, LOW);
+  int joy[3] = {analogRead(A0),analogRead(A1),digitalRead(2)};
+  radio.write(&joy, sizeof(joy));
+  if(!radio.write(&joy, sizeof(joy))){
+    Serial.println("Failed");
+    Serial.println();  
+  }else{
+    Serial.println("Proceeded"); 
+    Serial.println(); 
   }
 }
