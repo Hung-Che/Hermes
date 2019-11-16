901eced5f257aac17c590b6a73987c4142ebd6bb
Hung Che <chedaihung4864@gmail.com>
2019-11-16 16:28:36 -0600

    Update Wall-e.ino

--- Wall-e/Wall-e.ino
+++ Wall-e/Wall-e.ino
@@ -1,20 +1,32 @@
+#include <LedControl.h>
 #include <Servo.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include "binary.h"
 
+LedControl lc = LedControl(6,5,4,1); //DIN,CLK,CS
 RF24 radio(7,8);
 const byte address[6] = "00001";
 Servo left;
 Servo right;
 int joy[3]={90,90,1};
+unsigned long runTime=0;
 
-byte hf[8]= {B00111100,B01000010,B10100101,B10000001,B10100101,B10011001,B01000010,B00111100};
+byte up_face_norm = B00100100;
+byte up_face_blink[4] = {B00000000, B00100100, B01011010, B00000000};
+byte low_face_norm[4] = {B00000000, B01000010, B01000010, B00111100};
+byte low_face_laugh[4] = {B00000000, B01111110, B01000010, B00111100};
+byte heart_hollow[8] = {B00000000, B01100110, B10000001, B10000001, B10000001, B01000010, B00100100, B00011000};
+byte heart_full[8] = {B00000000, B01100110, B11111111, B11111111, B11111111, B01111110, B00111100, B00011000};
 
 void setup() {  
   left.attach(10);
   right.attach(3);
   Serial.begin(9600);
+  lc.shutdown(0, false);
+  lc.setIntensity(0,5);
+  lc.clearDisplay(0);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
@@ -28,10 +40,35 @@ void loop() {
     int y = map(joy[0]+joy[1],512,1536,180,0);
     right.write(x<=100&&x>=85?90:x);
     left.write(y<=100&&y>=85?90:y);
-    if(joy[2]==0){
-      tone(2,100);
-    }else if(joy[2]==1){
-      noTone(2);
+    upperFace();
+  }
+  
+}
+void upperFace(){
+  if(millis()>runTime+100){
+      runTime = millis();
+    for(int j = 0; j<4; j++){
+      lc.setColumn(0,1,up_face_blink[j]);
     }
+    lowerFace();
+  }else{
+    for(int i = 0; i<4; i++){
+      lc.setColumn(0, i, up_face_norm);
+    }
+    lowerFace();
+  }
+}
+
+void lowerFace(){
+  if(joy[2]==0){
+    for(int i = 0; i<4; i++){
+      lc.setColumn(0, 7-i, low_face_laugh[3-i]);
+    }
+    tone(2,100);
+  }else if(joy[2]==1){
+    for(int j = 0; j<4; j++){
+      lc.setColumn(0, 7-j, low_face_norm[3-j]);
+    }
+    noTone(2);
   }
 }
