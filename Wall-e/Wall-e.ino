e8882f3cdd316192671f0931809f327de9b1edcc
Hung Che <chedaihung4864@gmail.com>
2019-11-15 22:42:42 -0600

    Update Wall-e.ino

--- Wall-e/Wall-e.ino
+++ Wall-e/Wall-e.ino
@@ -7,25 +7,31 @@ RF24 radio(7,8);
 const byte address[6] = "00001";
 Servo left;
 Servo right;
-int joy[2]={90,90};
+int joy[3]={90,90,1};
 
 byte hf[8]= {B00111100,B01000010,B10100101,B10000001,B10100101,B10011001,B01000010,B00111100};
 
 void setup() {  
-  left.attach(3);
-  right.attach(10);
+  left.attach(10);
+  right.attach(3);
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
+
 void loop() {
   if(radio.available()){
     radio.read(&joy, sizeof(joy));
-    int x = map(joy[0]+joy[1],512,1536,0,180);
-    int y = map(joy[0]-joy[1],-512,512,180,0);
+    int x = map(joy[0]-joy[1],-512,512,0,180);
+    int y = map(joy[0]+joy[1],512,1536,180,0);
     right.write(x<=100&&x>=85?90:x);
     left.write(y<=100&&y>=85?90:y);
+    if(joy[2]==0){
+      tone(2,100);
+    }else if(joy[2]==1){
+      noTone(2);
+    }
   }
 }
